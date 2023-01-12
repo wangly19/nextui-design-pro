9a819ad49d87b5572623be692bf20a272fcec76b
wangly19 <1418202350@qq.com>
2023-01-12 23:45:50 +0800

    feat: create navbar components.

---
+++ app/next/layout/avatar-bar/icon.tsx
@@ -0,0 +1,11 @@
+import { styled } from '@nextui-org/react'
+
+
+export const Icon = styled('span', {
+  color: '$accents6',
+  cursor: 'pointer',
+  transition: '0.8s',
+  '&:hover': {
+    opacity: '0.5'
+  }
+})
---
+++ app/next/layout/avatar-bar/index.tsx
@@ -0,0 +1,83 @@
+import { Navbar, Dropdown, Avatar, Input } from "@nextui-org/react";
+import { Icon } from "./icon";
+import { Setting, ShieldDone, Chart, Search } from "react-iconly";
+import { NotificationDown } from "./notification";
+import { Spotlight, SpotlightInstance } from "@/components/spotlight";
+import { useRef } from "react";
+import { useToast } from "components/toast";
+import { LightMode } from "./mode";
+import { UserPopover } from "./user";
+
+export const AvatarBar = () => {
+  const spotlightRef = useRef<SpotlightInstance>(null);
+  const { add } = useToast();
+
+  return (
+    <Navbar.Content
+      gap={14}
+      css={{
+        "@xsMax": {
+          w: "100%",
+          jc: "space-between",
+        },
+      }}
+    >
+      <Spotlight ref={spotlightRef} />
+      <Navbar.Item
+        css={{
+          "@xsMax": {
+            w: "100%",
+            jc: "center",
+          },
+        }}
+      >
+        <Input
+          contentLeft={
+            <Icon
+              as={Search}
+              style={{
+                width: 20,
+                height: 20,
+              }}
+            />
+          }
+          contentLeftStyling={false}
+          css={{
+            w: "100%",
+            "@xsMax": {
+              mw: "300px",
+            },
+            "& .nextui-input-content--left": {
+              h: "100%",
+              ml: "$4",
+              dflex: "center",
+            },
+          }}
+          placeholder="Search..."
+        />
+      </Navbar.Item>
+      <LightMode />
+      <NotificationDown />
+
+      <Navbar.Item
+        onClick={() =>
+          add({
+            type: "success",
+            title: "title",
+            description: "description",
+          })
+        }
+      >
+        <Icon as={Chart} set="bold" />
+      </Navbar.Item>
+      <Navbar.Item>
+        <Icon as={ShieldDone} set="bold"></Icon>
+      </Navbar.Item>
+      <Navbar.Item>
+        <Icon as={Setting} set="bold"></Icon>
+      </Navbar.Item>
+
+      <UserPopover />
+    </Navbar.Content>
+  );
+};
