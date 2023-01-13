c6db2b6a2c554382a30554a565970da8b6a928ce
wangly19 <1418202350@qq.com>
2023-01-13 23:26:44 +0800

    chore: perfect flex & flex-box components.

--- app/next/components/flex.tsx
+++ app/next/components/flex.tsx
@@ -1,7 +1,20 @@
-import { styled, CSS } from "@nextui-org/react";
+import * as React from "react";
+import { styled } from "@nextui-org/react";
 
-export const Flex = styled('div', {
-  display: 'flex',
-  justifyContent: 'flex-start',
-  alignItems: 'center'
-})
+export const Flex = styled("div", {
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+});
+
+export const FlexBox: React.FC<
+  {
+    children?: React.ReactNode;
+    css?: React.ComponentProps<typeof Flex>["css"]
+  } & React.ComponentProps<typeof Flex>["css"]
+> = ({ children, css, ...styledProps }) => {
+  return <Flex css={{
+    ...css,
+    ...styledProps
+  }}>{children}</Flex>;
+};
