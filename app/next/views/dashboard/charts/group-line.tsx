f6a73f2eae0a9758577b381d77508b54c39d5425
wangly19 <1418202350@qq.com>
2023-01-18 14:33:35 +0800

    feat: add group-line charts example.

---
+++ app/next/views/dashboard/charts/group-line.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData
+} from "chart.js";
+import { Card, Text, useTheme } from "@nextui-org/react";
+import { Bar } from "react-chartjs-2";
+import { setLegendSpacingPlugin, setLegendStyle } from './config'
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+const defineConfig = {
+  borderWidth: 2,
+  borderRadius: 5,
+  borderSkipped: false,
+}
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data: ChartData<'bar', number[], string> = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: "#F7BA1E",
+      borderColor: "#F7BA1E",
+      stack: "Stack 0",
+      ...defineConfig,
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: "#14C9C9",
+      borderColor: "#14C9C9",
+      stack: "Stack 0",
+      ...defineConfig,
+    },
+    {
+      label: "Dataset 3",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: "#165DFF",
+      borderColor: "#165DFF",
+      stack: "Stack 1",
+      ...defineConfig,
+    },
+  ],
+};
+
+export const GroupLineCharts = () => {
+
+  const { theme  } = useTheme()
+
+  return (
+    <Card isHoverable variant="flat" css={{ p: "$6" }}>
+      <Card.Header>
+        <Text b>GMV</Text>
+      </Card.Header>
+      <Card.Body
+        css={{
+          py: 0,
+        }}
+      >
+        <Bar
+          plugins={setLegendSpacingPlugin}
+          options={{
+            plugins: {
+              legend: setLegendStyle,
+            },
+            responsive: true,
+            interaction: {
+              mode: "index" as const,
+              intersect: false,
+            },
+            scales: {
+              y: {
+                min: -1000,
+                max: 1000,
+                stacked: true,
+                title: {
+                  display: true,
+                  text: "Unit(W)",
+                  color: theme?.colors.primary.value,
+                  font: {
+                    weight: "bold",
+                  },
+                },
+                border: {
+                  dash: [10, 10],
+                  width: 0,
+                  color: "red",
+                },
+                grid: {
+                  color: theme?.colors.neutralBorder.value,
+                  lineWidth: 1.5,
+                },
+              },
+              x: {
+                stacked: true,
+                border: {
+                  display: false,
+                },
+                grid: {
+                  display: false,
+                },
+              },
+            },
+          }}
+          data={data}
+        />
+      </Card.Body>
+    </Card>
+  );
+};
